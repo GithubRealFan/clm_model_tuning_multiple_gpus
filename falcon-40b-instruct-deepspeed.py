b1c82b8fdbf400d2e06dda19a4635b3957f4c1e9
GithubRealFan <christianlee92227@gmail.com>
2023-07-17 09:24:50 -0400

    updated deepspped

--- falcon-40b-instruct-deepspeed.py
+++ falcon-40b-instruct-deepspeed.py
@@ -2,51 +2,55 @@ import torch
 import deepspeed
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# Model and tokenizer
 model_name = "tiiuae/falcon-40b-instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
 
-print("Tokenizer Loaded!")
+# DeepSpeed configuration
+deepspeed_config = {
+    "train_batch_size": 18,
+    "gradient_accumulation_steps": 1,
+    "optimizer": {
+        "type": "Adam",
+        "params": {
+            "lr": 5e-6,
+            "weight_decay": 0.0
+        }
+    },
+    "fp16": {
+        "enabled": True,
+        "loss_scale": 0,
+        "loss_scale_window": 1000,
+        "hysteresis": 2,
+        "min_loss_scale": 1
+    },
+    "steps_per_print": 80,
+    "wall_clock_breakdown": True
+}
 
-deepspeed_config_file = "conf/deepspeed_config.json"
+# Initialize DeepSpeed
+model, _, _, _ = deepspeed.initialize(model=model, config_params=deepspeed_config)
 
-# Initialize the model and tokenizer using DeepSpeed with model parallelism
-model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
-print("Model Loaded!")
+# DeepSpeed pipeline
+pipeline = torch.distributed.DistributedDataParallel(model, device_ids=[torch.cuda.current_device()])
 
-model, _, _, _ = deepspeed.initialize(model=model, config_params=deepspeed_config_file)
+# Input text
+input_text = "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:"
 
-print("Deepspeed Initialized!")
+# Tokenize input text
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(pipeline.device)
 
-# Create the pipeline
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="auto",
-)
-
-print("Pipeline Completed!")
-
-# Input text for generation
-input_text = (
-    "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. "
-    "Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\n"
-    "Daniel: Hello, Girafatron!\nGirafatron:"
-)
-
-# Generate text using DeepSpeed-powered pipeline
-sequences = pipeline(
-    input_text,
+# Generate output text
+output_ids = pipeline.generate(
+    input_ids,
     max_length=200,
     do_sample=True,
     top_k=10,
     num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
+    eos_token_id=tokenizer.eos_token_id
 )
 
-print("Text Output :")
-
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+# Decode and print the generated text
+output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+print("Result:", output_text)
